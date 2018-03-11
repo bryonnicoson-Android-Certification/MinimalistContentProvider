bc9567b20125f86f0fe79d4b750d91ba59dd1e2e
Bryon Nicoson <bryon.nicoson@gmail.com>
2018-03-11 11:11:29 -0500

    UI layout wired

--- app/src/main/java/com/bryonnicoson/minimalistcontentprovider/MainActivity.java
+++ app/src/main/java/com/bryonnicoson/minimalistcontentprovider/MainActivity.java
@@ -2,12 +2,37 @@ package com.bryonnicoson.minimalistcontentprovider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private TextView mTextview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextview = findViewById(R.id.textview);
+    }
+
+    public void onClickDisplayEntries(View view){
+
+        switch(view.getId()){
+            case R.id.button_display_all:
+                Log.d(TAG, "Yay, " + R.id.button_display_all + " was clicked!");
+                break;
+            case R.id.button_display_first:
+                Log.d(TAG, "Yay, " + R.id.button_display_first + " was clicked!");
+                break;
+            default:
+                Log.wtf(TAG, "Error. This should never happen.");
+        }
+
+        mTextview.append("\nGo, Cubs, Go!");
     }
 }
